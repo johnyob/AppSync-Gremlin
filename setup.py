19af8007996bbcc047793e506df694e7803378c9
johnyob <johnyob132@gmail.com>
2019-09-18 12:24:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+requires = [
+    "gremlinpython"
+]
+
+packages = [
+    "appsync_gremlin",
+    "appsync_gremlin.filter",
+    "appsync_gremlin.helpers",
+    "appsync_gremlin.resolver"
+]
+
+setuptools.setup(
+    name="appsync-gremlin",
+    version="0.0.1",
+    author="Alistair O'Brien",
+    author_email="alistair@duneroot.co.uk",
+    description="A simple Python interface for AppSync resolvers and Gremlin traversals.",
+    long_description=long_description,
+    include_package_data=True,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=packages,
+    install_requires=requires,
+)
