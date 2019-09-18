9067cbec6a4442e143bc9c5e6db9f662ecf4001e
johnyob <johnyob132@gmail.com>
2019-09-18 14:11:36 +0100

    Update AbstractResolver.py.

--- appsync_gremlin/resolver/AbstractResolver.py
+++ appsync_gremlin/resolver/AbstractResolver.py
@@ -2,8 +2,35 @@ from abc import ABC as Abstract, abstractmethod, abstractproperty
 from typing import Dict, Any, List
 
 from gremlin_python.process.graph_traversal import GraphTraversal, unfold
+from gremlin_python.process.traversal import T
 
 from appsync_gremlin.resolver.ResolverInput import ResolverInput
+from appsync_gremlin.filter.Filter import apply_filters
+
+
+def format_key(key: Any) -> Any:
+    """
+
+    :param key:
+    :return:
+    """
+
+    if isinstance(key, T):
+        return key.name
+
+    return key
+
+
+def format_value_map(value_map: Dict) -> Dict:
+    """
+
+    :param value_map:
+    :return:
+    """
+
+    return {
+        format_key(k): v for k, v in value_map.items()
+    }
 
 
 class AbstractResolver(Abstract):
@@ -41,6 +68,20 @@ class AbstractResolver(Abstract):
         pass
 
     @abstractmethod
+    def format_response(self, response: Any) -> Any:
+        """
+        This method is used for GremlinPython to JSON serialization. For example, when retrieving vertex
+        id, GremlinPython returns a dict with an enum -> Any mapping. The enum cannot be serialized (in
+        AWS Lambda) so we use this method to implement the required serialization.
+
+        :param response: (Any)
+        :return: (Any)
+        """
+
+        pass
+
+
+    @abstractmethod
     def get_traversal(self, traversal: GraphTraversal, resolver_input: ResolverInput) -> GraphTraversal:
         """
         Returns the Gremlin graph traversal for the resolver. This is implemented by the concrete class.
@@ -71,6 +112,17 @@ class AbstractResolver(Abstract):
 
 class VertexListFieldResolver(AbstractResolver):
 
+    def format_response(self, response: List[Dict]) -> List[Dict]:
+        """
+        Iterates through the response list, applying format_value_map on the value map. This serializes the value map
+        into something that can be returned by a AWS Lambda function.
+
+        :param response: (list)
+        :return: (list)
+        """
+
+        return [format_value_map(value_map) for value_map in response]
+
     def handle(self, traversal: GraphTraversal, resolver_input: ResolverInput) -> List[Dict]:
         """
 
@@ -83,10 +135,20 @@ class VertexListFieldResolver(AbstractResolver):
 
         traversal = self.get_traversal(traversal, resolver_input)
 
-        return apply_filters(traversal, input_dict).valueMap().by(unfold()).toList()
+        return self.format_response(apply_filters(traversal, input_dict).valueMap().by(unfold()).toList())
 
 
 class VertexFieldResolver(AbstractResolver):
+
+    def format_response(self, response: Dict) -> Dict:
+        """
+        Applies the format_value_map function to the response dict (a value map).
+
+        :param response: (dict)
+        :return: (dict)
+        """
+
+        return format_value_map(response)
 
     def handle(self, traversal: GraphTraversal, resolver_input: ResolverInput) -> Dict:
         """
@@ -99,12 +161,22 @@ class VertexFieldResolver(AbstractResolver):
         traversal = self.get_traversal(traversal, resolver_input).valueMap().by(unfold())
 
         if traversal.hasNext():
-            return traversal.next()
+            return self.format_response(traversal.next())
 
         return None
 
 
 class CalculatedFieldResolver(AbstractResolver):
+
+    def format_response(self, response: Any) -> None:
+        """
+        No formatting required for calculated fields.
+
+        :param response: (Any)
+        :return: (None)
+        """
+
+        return None
 
     def handle(self, traversal: GraphTraversal, resolver_input: ResolverInput) -> Any:
         """
