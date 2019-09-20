5f29346a4e311b0c926fa6eb75322a81be2f7dbe
johnyob <johnyob132@gmail.com>
2019-09-20 13:59:42 +0100

    Update appsync_gremlin.filter. New imports for new filtering system and created some basic ScalarFilters that are commonly used.

--- appsync_gremlin/filter/__init__.py
+++ appsync_gremlin/filter/__init__.py
@@ -1,1 +1,104 @@
-from appsync_gremlin.filter.Filter import apply_filters
+from typing import Dict, Callable
+
+from gremlin_python.process.traversal import (
+    eq, neq, lt, lte, gt, gte, within, without, startingWith, endingWith, containing,
+    notStartingWith, notEndingWith, notContaining
+)
+
+from appsync_gremlin.filter.AbstractFilter import (
+    AbstractFilter, ScalarFilter, VertexFilter, RelationshipDirection, RelationshipFilter
+)
+
+
+class IDScalarFilter(ScalarFilter):
+
+    @property
+    def sub_filters(self) -> Dict[str, Callable]:
+
+        return {
+            "ne": neq,
+            "eq": eq,
+            "in": within,
+            "not_in": without
+        }
+
+
+class StringScalarFilter(ScalarFilter):
+
+    @property
+    def sub_filters(self) -> Dict[str, Callable]:
+
+        return {
+            "ne": neq,
+            "eq": eq,
+            "in": within,
+            "not_in": without,
+            "contains": containing,
+            "not_contains": notContaining,
+            "begins_with": startingWith,
+            "not_begins_with": notStartingWith,
+            "ends_with": endingWith,
+            "not_ends_with": notEndingWith
+        }
+
+
+class IntScalarFilter(ScalarFilter):
+
+    @property
+    def sub_filters(self) -> Dict[str, Callable]:
+
+        return {
+            "ne": neq,
+            "eq": eq,
+            "le": lte,
+            "lt": lt,
+            "ge": gte,
+            "gt": gt,
+            "in": within,
+            "not_in": without
+        }
+
+
+class FloatScalarFilter(ScalarFilter):
+
+    @property
+    def sub_filters(self) -> Dict[str, Callable]:
+
+        return {
+            "ne": neq,
+            "eq": eq,
+            "le": lte,
+            "lt": lt,
+            "ge": gte,
+            "gt": gt,
+            "in": within,
+            "not_in": without
+        }
+
+
+class BooleanScalarFilter(ScalarFilter):
+
+    @property
+    def sub_filters(self) -> Dict[str, Callable]:
+
+        return {
+            "eq": eq,
+            "ne": neq
+        }
+
+
+class DateTimeScalarFilter(ScalarFilter):
+
+    @property
+    def sub_filters(self) -> Dict[str, Callable]:
+
+        return {
+            "ne": neq,
+            "eq": eq,
+            "le": lte,
+            "lt": lt,
+            "ge": gte,
+            "gt": gt,
+            "in": within,
+            "not_in": without
+        }
