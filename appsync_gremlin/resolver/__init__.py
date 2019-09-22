449f0aac7a55b9ec2206928a9d183dca05de0b0f
johnyob <johnyob132@gmail.com>
2019-09-22 16:30:34 +0100

    Update appsync_gremlin.resolver

--- appsync_gremlin/resolver/__init__.py
+++ appsync_gremlin/resolver/__init__.py
@@ -1,2 +1,6 @@
-from appsync_gremlin.resolver.AbstractResolver import AbstractResolver, VertexFieldResolver, VertexListFieldResolver, CalculatedFieldResolver
+from appsync_gremlin.resolver.Resolver import (
+    TraversalFilterFunction, VertexListFieldResolverFunction, VertexFieldResolverFunction, CalculatedFieldResolverFunction,
+    ResolverFunction,
+    vertex_field_resolver, vertex_list_field_resolver, calculated_field_resolver
+)
 from appsync_gremlin.resolver.ResolverInput import ResolverInput
