ea6d7dca732fc7b6ed650e7a19d4f90b42792f2e
johnyob <johnyob132@gmail.com>
2019-09-22 16:31:27 +0100

    Update appsync_gremlin

--- appsync_gremlin/__init__.py
+++ appsync_gremlin/__init__.py
@@ -1,8 +1,14 @@
 from appsync_gremlin.helpers import AppSyncException
-from appsync_gremlin.resolver import ResolverInput, AbstractResolver, VertexFieldResolver, VertexListFieldResolver, CalculatedFieldResolver
+from appsync_gremlin.resolver import (
+    TraversalFilterFunction, VertexListFieldResolverFunction, VertexFieldResolverFunction,
+    CalculatedFieldResolverFunction,
+    ResolverFunction,
+    vertex_field_resolver, vertex_list_field_resolver, calculated_field_resolver,
+    ResolverInput
+)
 from appsync_gremlin.AppSync import AppSync
 from appsync_gremlin.filter import (
-    AbstractFilter, ScalarFilter, VertexFilter, RelationshipDirection, RelationshipFilter,
-    IDScalarFilter, StringScalarFilter, BooleanScalarFilter, DateTimeScalarFilter,
-    FloatScalarFilter, IntScalarFilter
+    RelationshipDirection, scalar_filter, vertex_filter, relationship_filter,
+    TraversalFilterFunction, Relationship, FilterFunction, NameFunction,
+    id_filter, string_filter, int_filter, float_filter, date_time_filter, boolean_filter, enum_filter
 )
