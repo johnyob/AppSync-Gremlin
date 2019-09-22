e31737cb1571ea76d68223cb57733dd0bdd9d42d
johnyob <johnyob132@gmail.com>
2019-09-22 16:30:45 +0100

    Update appsync_gremlin.filter

--- appsync_gremlin/filter/__init__.py
+++ appsync_gremlin/filter/__init__.py
@@ -1,104 +1,95 @@
-from typing import Dict, Callable
-
 from gremlin_python.process.traversal import (
     eq, neq, lt, lte, gt, gte, within, without, startingWith, endingWith, containing,
     notStartingWith, notEndingWith, notContaining
 )
 
-from appsync_gremlin.filter.AbstractFilter import (
-    AbstractFilter, ScalarFilter, VertexFilter, RelationshipDirection, RelationshipFilter
+from appsync_gremlin.filter.Filter import (
+    RelationshipDirection, scalar_filter, vertex_filter, relationship_filter,
+    TraversalFilterFunction, Relationship, FilterFunction, NameFunction
 )
 
 
-class IDScalarFilter(ScalarFilter):
-
-    @property
-    def sub_filters(self) -> Dict[str, Callable]:
-
-        return {
-            "ne": neq,
-            "eq": eq,
-            "in": within,
-            "not_in": without
-        }
+@scalar_filter
+def id_filter():
+    return {
+        "ne": neq,
+        "eq": eq,
+        "in": within,
+        "not_in": without
+    }
 
 
-class StringScalarFilter(ScalarFilter):
-
-    @property
-    def sub_filters(self) -> Dict[str, Callable]:
-
-        return {
-            "ne": neq,
-            "eq": eq,
-            "in": within,
-            "not_in": without,
-            "contains": containing,
-            "not_contains": notContaining,
-            "begins_with": startingWith,
-            "not_begins_with": notStartingWith,
-            "ends_with": endingWith,
-            "not_ends_with": notEndingWith
-        }
+@scalar_filter
+def string_filter():
+    return {
+        "ne": neq,
+        "eq": eq,
+        "in": within,
+        "not_in": without,
+        "contains": containing,
+        "not_contains": notContaining,
+        "begins_with": startingWith,
+        "not_begins_with": notStartingWith,
+        "ends_with": endingWith,
+        "not_ends_with": notEndingWith
+    }
 
 
-class IntScalarFilter(ScalarFilter):
-
-    @property
-    def sub_filters(self) -> Dict[str, Callable]:
-
-        return {
-            "ne": neq,
-            "eq": eq,
-            "le": lte,
-            "lt": lt,
-            "ge": gte,
-            "gt": gt,
-            "in": within,
-            "not_in": without
-        }
+@scalar_filter
+def int_filter():
+    return {
+        "ne": neq,
+        "eq": eq,
+        "le": lte,
+        "lt": lt,
+        "ge": gte,
+        "gt": gt,
+        "in": within,
+        "not_in": without
+    }
 
 
-class FloatScalarFilter(ScalarFilter):
-
-    @property
-    def sub_filters(self) -> Dict[str, Callable]:
-
-        return {
-            "ne": neq,
-            "eq": eq,
-            "le": lte,
-            "lt": lt,
-            "ge": gte,
-            "gt": gt,
-            "in": within,
-            "not_in": without
-        }
+@scalar_filter
+def float_filter():
+    return {
+        "ne": neq,
+        "eq": eq,
+        "le": lte,
+        "lt": lt,
+        "ge": gte,
+        "gt": gt,
+        "in": within,
+        "not_in": without
+    }
 
 
-class BooleanScalarFilter(ScalarFilter):
-
-    @property
-    def sub_filters(self) -> Dict[str, Callable]:
-
-        return {
-            "eq": eq,
-            "ne": neq
-        }
+@scalar_filter
+def boolean_filter():
+    return {
+        "eq": eq,
+        "ne": neq
+    }
 
 
-class DateTimeScalarFilter(ScalarFilter):
+@scalar_filter
+def date_time_filter():
+    return {
+        "ne": neq,
+        "eq": eq,
+        "le": lte,
+        "lt": lt,
+        "ge": gte,
+        "gt": gt,
+        "in": within,
+        "not_in": without
+    }
 
-    @property
-    def sub_filters(self) -> Dict[str, Callable]:
 
-        return {
-            "ne": neq,
-            "eq": eq,
-            "le": lte,
-            "lt": lt,
-            "ge": gte,
-            "gt": gt,
-            "in": within,
-            "not_in": without
-        }
+@scalar_filter
+def enum_filter():
+    return {
+        "eq": eq,
+        "neq": neq,
+        "in": within,
+        "not_in": without
+    }
