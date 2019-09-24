7e03ee98ea5983c082a352dc5efd27692f0cd43d
johnyob <johnyob132@gmail.com>
2019-09-25 00:02:56 +0100

    Update appsync_gremlin.resolver. Added mutation resolver

--- appsync_gremlin/resolver/__init__.py
+++ appsync_gremlin/resolver/__init__.py
@@ -1,6 +1,6 @@
 from appsync_gremlin.resolver.Resolver import (
     TraversalFilterFunction, VertexListFieldResolverFunction, VertexFieldResolverFunction, CalculatedFieldResolverFunction,
     ResolverFunction,
-    vertex_field_resolver, vertex_list_field_resolver, calculated_field_resolver
+    vertex_field_resolver, vertex_list_field_resolver, calculated_field_resolver, mutation_resolver
 )
 from appsync_gremlin.resolver.ResolverInput import ResolverInput
