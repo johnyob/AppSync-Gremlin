b78c6d00e2d6cdac9d7b727280b49306d9d1e68e
johnyob <johnyob132@gmail.com>
2019-09-26 10:40:15 +0100

    Update appsync_gremlin.filter

--- appsync_gremlin/filter/__init__.py
+++ appsync_gremlin/filter/__init__.py
@@ -6,7 +6,8 @@ from gremlin_python.process.traversal import (
 from appsync_gremlin.filter.Filter import (
     RelationshipDirection, scalar_filter, vertex_filter, relationship_filter,
     TraversalFilterFunction, Relationship, FilterFunction, NameFunction,
-    name
+    name,
+    EdgeDirection, edge_filter
 )
 
 
