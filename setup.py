00dac7ee4c89a7a609194c2a8e8494e6fe891b0e
johnyob <johnyob132@gmail.com>
2019-09-26 16:04:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ packages = [
 
 setuptools.setup(
     name="appsync-gremlin",
-    version="0.0.16",
+    version="0.0.17",
     author="Alistair O'Brien",
     author_email="alistair@duneroot.co.uk",
     description="A simple Python interface for AppSync resolvers and Gremlin traversals.",
